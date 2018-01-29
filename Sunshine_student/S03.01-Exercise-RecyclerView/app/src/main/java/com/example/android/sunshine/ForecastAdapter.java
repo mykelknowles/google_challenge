a4d6e3959f4225f68ae0fa2b97f81a1af7170bf7
MykelKnowles <mknowles@soggastudios.com>
2018-01-28 23:05:32 -0800

    Lesson 4 Step 23 S03.01-Exercise-RecyclerView Completed

---
+++ Sunshine_student/S03.01-Exercise-RecyclerView/app/src/main/java/com/example/android/sunshine/ForecastAdapter.java
@@ -0,0 +1,58 @@
+package com.example.android.sunshine;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+/**
+ * Created by Mykel on 1/28/2018.
+ */
+
+public class ForecastAdapter extends RecyclerView.Adapter<ForecastAdapter.ForecastAdapterViewHolder> {
+
+    private String[] mWeatherData;
+
+    public ForecastAdapter() {
+
+    }
+
+    public class ForecastAdapterViewHolder extends RecyclerView.ViewHolder {
+        public final TextView mWeatherTextView;
+
+        public ForecastAdapterViewHolder(View view) {
+            super(view);
+            mWeatherTextView = (TextView) view.findViewById(R.id.tv_weather_data);
+        }
+    }
+
+    @Override
+    public ForecastAdapterViewHolder onCreateViewHolder(ViewGroup viewGroup, int viewType) {
+        Context context = viewGroup.getContext();
+        int layoutIdForListItem = R.layout.forecast_list_item;
+        LayoutInflater inflator = LayoutInflater.from(context);
+        boolean shouldAttachToParentImmdiately = false;
+
+        View view = inflator.inflate(layoutIdForListItem, viewGroup, shouldAttachToParentImmdiately);
+        return new ForecastAdapterViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(ForecastAdapterViewHolder forecastAdapterViewHolder, int position) {
+        String weatherForThisDay = mWeatherData[position];
+        forecastAdapterViewHolder.mWeatherTextView.setText(weatherForThisDay);
+    }
+
+    @Override
+    public int getItemCount() {
+        if (null == mWeatherData) { return 0; }
+        return mWeatherData.length;
+    }
+
+    public void setWeatherData(String[] weatherData) {
+        mWeatherData = weatherData;
+        notifyDataSetChanged();
+    }
+}
